f9aaa832d31435f9088102a525f5412e1ce9c007
William Darin Campbell <animusdrogai@gmail.com>
2018-01-14 21:36:46 -0500

    Cleaned up classes and created parent classes to remove repitition in child classes

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine
+{
+    public class LivingCreature
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+
+    }
+}
--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,9 +1,8 @@
 ﻿namespace Engine
 {
-    public class Player
+    public class Player : LivingCreature
     {
-        public int CurrentHitPoints { get; set; }
-        public int MaximumHitPoints { get; set; }
+        
         public int Gold { get; set; }
         public int ExperiancePoints { get; set; }
         public int Level { get; set; }
