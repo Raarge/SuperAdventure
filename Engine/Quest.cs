a98956c43452322e0509589dbe3cf48de4ab1550
William Darin Campbell <animusdrogai@gmail.com>
2018-01-14 22:50:28 -0500

    Added addiitonal parameters to Location.cs and Quest.cs updated class constructors

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -7,14 +7,16 @@
         public string Description { get; set; }
         public int RewardExperiance { get; set; }
         public int RewardGold { get; set; }
+        public Item RewardItem { get; set; }
 
-        public Quest(int id, string name, string description, int rewardExperiancePoints, int rewardGold)
+        public Quest(int id, string name, string description, int rewardExperiancePoints, int rewardGold, Item rewardItem = null)
         {
             ID = id;
             Name = name;
             Description = description;
             RewardExperiance = rewardExperiancePoints;
             RewardGold = rewardGold;
+            RewardItem = rewardItem;
         }
     }
 }
